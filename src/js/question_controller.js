daedaa482e34aa2252f81fb307c668dc9d463fc1
Jonathan Sundqvist <jonathan+git@argpar.se>
2025-04-11 13:57:49 +0200

    Only be able to proceed if answer is checked

--- src/js/question_controller.js
+++ src/js/question_controller.js
@@ -12,7 +12,7 @@ class QuestionController extends Controller {
   ]
 
   async initialize() {
-    await this.loadJSON()
+    await this.loadQuestions()
   }
 
   connect() {
@@ -32,20 +32,51 @@ class QuestionController extends Controller {
     this.numberTarget.innerHTML = this.currentQuestion + 1
   }
 
+  setExistingAnswer() {
+    let answer = this.userAnswers[this.currentQuestion]
+    this.answerTargets.map((target) => {
+      if (answer == undefined) {
+        target.checked = false
+      } else if (parseInt(target.value) ==  answer || isNaN(answer)) {
+        target.checked = true
+      } else {
+        // target does not match, do nothing.
+      }
+    })
+  }
+
   next() {
+    if (!this.canProceed()) {
+      return
+    }
     this.setQuestion(this.currentQuestion + 1)
+    this.setExistingAnswer()
+    this.canProceed()
+    console.log(this.userAnswers)
   }
 
   previous() {
     this.setQuestion(this.currentQuestion - 1)
+    this.setExistingAnswer()
+    console.log(this.userAnswers)
+  }
+
+  canProceed() {
+    let isChecked = this.answerTargets.filter((target) => target.checked == true).length >= 1
+    if (this.userAnswers[this.currentQuestion] != undefined || isChecked ) {
+      this.nextTarget.removeAttribute("disabled")
+      return true
+    }
+    this.nextTarget.setAttribute("disabled", "disabled")
+    return false
   }
 
   answer(event) {
     this.userAnswers[this.currentQuestion] = parseInt(event.target.value)
-    this.next()
+    this.canProceed()
   }
 
-  async loadJSON() {
+  async loadQuestions() {
     try {
       const response = await fetch('./questions.json');
       const data = await response.json();
